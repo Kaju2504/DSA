21269b5b82bcace2b5e721208921a937041c4a9a
Kaju2504 <kashishraj042507@gmail.com>
2026-02-06 19:49:50 +0530

    Add files via upload

---
+++ pushandpop.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+#define n 5
+int stack[5];
+int top = -1;
+
+int push(int x) {
+    if (top == n - 1)
+        cout << "Stack Overflow\n";
+    else
+        stack[++top] = x;
+}
+
+int pop() {
+    if (top == -1)
+        cout << "Stack Underflow\n";
+    else
+        top--;
+}
+void display() {
+    if (top == -1) {
+        cout << "Stack is empty\n";
+        return;
+    }
+    for (int i = top; i >= 0; i--)
+        cout << stack[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+
+    pop();
+    display();
+}
